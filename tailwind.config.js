08ee333192ad76df1dffed937e3310eefd934511
Wilda <kharisma.2025@smkn1purwosari.sch.id>
2026-06-08 21:16:55 +0700

    index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,3 +12,18 @@ module.exports = {
     preflight: false,
   },
 };
+
+
+function dismissCookie() {
+    const banner = document.getElementById('cookie-banner');
+    
+    if (banner) {
+        // Menggunakan animasi transisi pudar dan turun ke bawah sebelum dibuang
+        banner.classList.add('opacity-0', 'pointer-events-none', 'translate-y-10');
+        
+        // Membuang elemen secara kekal daripada DOM selepas animasi tamat
+        setTimeout(() => {
+            banner.remove();
+        }, 300);
+    }
+}
